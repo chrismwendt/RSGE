bfd6c31cef3ccca25f83fddfe14642b26eb37f29
Chris Wendt <chrismwendt@gmail.com>
2013-12-24 18:26:31 -0500

    Create a wrapper for the database connection

--- main.js
+++ main.js
@@ -2,7 +2,32 @@ var mongoose = require('mongoose');
 var async = require('async');
 var rsNumber = require('./rs-number');
 
+var databaseName = 'rs';
+var collectionName = 'ge';
+
+var wrap = function(uri, callback) {
+    mongoose.connect(uri);
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+        console.log('Connected to ' + uri + '.');
+        console.log('Opened ' + uri + '.');
+        callback(function() {
+            db.close(function() {
+                console.log('Closed ' + uri + '.');
+                mongoose.disconnect(function() {
+                    console.log('Disconnected from ' + uri + '.');
+                });
+            });
+        });
+    });
+}
+
 var main = function() {
+    wrap('mongodb://localhost/' + databaseName, function(callback) {
+        console.log('insert prices');
+        callback();
+    });
 }
 
 main();
